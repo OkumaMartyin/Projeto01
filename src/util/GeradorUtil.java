feca68f7f3ade15ed3101061333d99c06e48e0a3
martyin.okuma <martyin.okuma@PLE111D16.alunos.sc.senac.br>
2021-08-02 21:51:54 -0300

    Projeto da faculdade Senac Palhoça. Aprendendo lógica com POO.

--- src/util/GeradorUtil.java
+++ src/util/GeradorUtil.java
@@ -1,14 +1,31 @@
 package util;
 
 
-public class GeradorUtil {
+public class GeradorUtil {  
+    
+    //devolve o valor string conforme a quantidade de número solicitado
+   public static String gerarNumero(int qtd){
+       String senha = "";
+       int numero;
+       for (int i = 0; i < qtd; i++) {
+           numero = (int)(Math.random() * 10);
+           senha += numero;
+       }
+       return senha;
+   } 
+     
    public double gerarSalario(){
        int numero = (int) (Math.random()*100); //ta forçando a ser inteiro
        return numero;
    }
    
+     public static String gerarCPF(){
+       return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + "-" + gerarNumero(2);
+   }
+   
     public static void main(String[] args) {
       GeradorUtil objetoDessaClasse = new GeradorUtil();
-        System.out.println("Salario: "+objetoDessaClasse.gerarSalario());
+      String cpf = GeradorUtil.gerarCPF();
+      System.out.println("CPF " + cpf);
     }
 }
